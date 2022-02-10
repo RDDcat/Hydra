f3a72cbd2899d0ad6c63c8795d27f06159028ca5
nulzi <kimty131@naver.com>
2022-02-10 23:19:03 +0900

    [STUDY] C#
    Variable. Type. Function. Specifier

---
+++ My project/Assets/Scenes/CodeStudy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeStudy : MonoBehaviour
+{
+    //정수 자료형: sbyte(-128~127), byte(0~255), (u)short(2바이트), (u)int(4바이트), (u)long(8바이트)
+    //실수 자료형: float(f), double, decimal(m)
+    //문자 자료형: string, char
+    //bool
+    //casting: .ToString()(숫자>>문자), int.Parse()(문자>>숫자)
+
+    //범위 지정자(지역 변수, 전역 변수), 접근 지정자(private, public, protected), static
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
