169eee3cd0c329b50cdc8184d546868b2ca4f7ae
nulzi <kimty131@naver.com>
2022-02-21 16:52:49 +0900

    [STUDY] 2D_Game
    control Player with (w,a,s,d) or (up, down, right, left)

--- 2D_Study/Assets/Movement2D.cs
+++ 2D_Study/Assets/Movement2D.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
+    private float moveSpeed = 5.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
     private void Update()
     {
-        // new position = now + (vector*speed)
-        //transform.position = transform.position + new Vector3(1, 0, 0) * 1;
-        transform.position += Vector3.right * 1 * Time.deltaTime;
+        // Negative left, a : -1
+        // Positive right, d : 1
+        // Non : 0
+        float x = Input.GetAxisRaw("Horizontal"); //move left, right
+        // Negative down, s : -1
+        // Postive up, w : 1
+        // Non : 0
+        float y = Input.GetAxisRaw("Vertical"); //move up, down
+
+        moveDirection = new Vector3(x, y, 0);
+
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
