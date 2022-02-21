b5f6425839adc636a5ea4c2a3bd1f0bbed7904d2
nulzi <kimty131@naver.com>
2022-02-21 16:44:02 +0900

    [STUDY] 2D_Game
    continuous move

--- 2D_Study/Assets/Movement2D.cs
+++ 2D_Study/Assets/Movement2D.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
-    private void Awake()
+    private void Update()
     {
         // new position = now + (vector*speed)
-        transform.position = transform.position + new Vector3(1, 0, 0) * 1;
-        // transform.position += Vector3.right * 1;
+        //transform.position = transform.position + new Vector3(1, 0, 0) * 1;
+        transform.position += Vector3.right * 1 * Time.deltaTime;
     }
 }
