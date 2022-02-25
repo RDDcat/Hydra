3815e5c88f25e0c3c56988434e4e2014e7385164
nulzi <kimty131@naver.com>
2022-02-25 16:19:35 +0900

    [STUDY] 2D_Game
    Collider. Rigidbody2D.

--- 2D_Study/Assets/Movement2D.cs
+++ 2D_Study/Assets/Movement2D.cs
@@ -3,21 +3,22 @@ using UnityEngine;
 public class Movement2D : MonoBehaviour
 {
     private float moveSpeed = 5.0f;
-    private Vector3 moveDirection = Vector3.zero;
+    private Rigidbody2D rigid2D;
+
+    private void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-        // Negative left, a : -1
-        // Positive right, d : 1
-        // Non : 0
         float x = Input.GetAxisRaw("Horizontal"); //move left, right
-        // Negative down, s : -1
-        // Postive up, w : 1
-        // Non : 0
         float y = Input.GetAxisRaw("Vertical"); //move up, down
 
-        moveDirection = new Vector3(x, y, 0);
+        //새로운 위치 = 현재 위치 + (방향 * 속도)
+        //transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        // Rigidbody2D 컴포넌트에 있는 속력(velocity) 변수 설정
+        rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
     }
 }
