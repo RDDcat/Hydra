6f21db5dbf553ff638e2b65037ead0e9ec3adf99
nulzi <kimty131@naver.com>
2022-02-26 13:49:44 +0900

    [STUDY] 2D_Game
    spawn with spawnPoint and move

--- 2D_Study/Assets/Movement2D.cs
+++ 2D_Study/Assets/Movement2D.cs
@@ -3,22 +3,15 @@ using UnityEngine;
 public class Movement2D : MonoBehaviour
 {
     private float moveSpeed = 5.0f;
-    private Rigidbody2D rigid2D;
+    private Vector3 moveDirection;
 
-    private void Awake()
+    public void Setup(Vector3 direction)
     {
-        rigid2D = GetComponent<Rigidbody2D>();
+        moveDirection = direction;
     }
 
     private void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal"); //move left, right
-        float y = Input.GetAxisRaw("Vertical"); //move up, down
-
-        //새로운 위치 = 현재 위치 + (방향 * 속도)
-        //transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-        // Rigidbody2D 컴포넌트에 있는 속력(velocity) 변수 설정
-        rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
--- 2D_Study/Assets/ObjectSpawner.cs
+++ 2D_Study/Assets/ObjectSpawner.cs
@@ -3,44 +3,25 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject boxPrefab;
+    private int objectSpawnCount = 30;
+    [SerializeField]
+    private GameObject[] prefabArray;
+    [SerializeField]
+    private Transform[] spawnPointArray;
 
     private void Awake()
     {
-        //Instantiate(원본 오브젝트,위치,회전)
-        //위치만 따로 설정은 불가능하고 3개의 매개변수를 사용해야 한다
-        //Quaternion.identity : no change
-        //Instantiate(boxPrefab, new Vector3(3, 3, 0), Quaternion.identity);
-        //Instantiate(boxPrefab, new Vector3(-1, -2, 0), Quaternion.identity);
-
-        //회전하는 방식 1.Euler 2. Quaternion
-        //Quaternion rotation = Quaternion.Euler(0, 0, 10);
-        
-        //GameObject clone = Instantiate(boxPrefab, new Vector3(2, 1, 0), rotation);
-        //clone.name = "Box001";//이름 변경
-        //clone.GetComponent<SpriteRenderer>().color = Color.black;//색상 변경
-        //clone.transform.position = new Vector3(2, 1, 0);//위치 변경
-        //clone.transform.localScale = new Vector3(3, 2, 1);//크기 변경
-
-        //반복 생성
-        //for(int i = 0; i < 10; i++)
-        //{
-        //    Quaternion rotation = Quaternion.Euler(0, 0, 9*i);
-        //    Instantiate(boxPrefab, new Vector3(-5 + i, 0, 0), rotation);
-        //}
-
-        //이중반복문
-        for(int y = 0; y < 10; ++ y)
+       for ( int i = 0; i < objectSpawnCount; i++)
         {
-            for(int x = 0; x < 10; ++ x)
-            {
-                //if(x == y || x+y == 9)
-                if(x+y == 4 || x+y == 5)
-                {
-                    continue;
-                }
-                Instantiate(boxPrefab, new Vector3(-4.5f+ x, 4.5f - y, 0), Quaternion.identity);
-            }
+            int prefabIndex = Random.Range(0, prefabArray.Length);
+            int spawnIndex = Random.Range(0, spawnPointArray.Length);
+
+            Vector3 position = spawnPointArray[spawnIndex].position;
+
+            GameObject clone = Instantiate(prefabArray[prefabIndex], position, Quaternion.identity);
+
+            Vector3 moveDirection = (spawnIndex == 0 ? Vector3.right : Vector3.left);
+            clone.GetComponent<Movement2D>().Setup(moveDirection);
         }
     }
 }
